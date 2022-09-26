66850443536e50968bbbbd8384b83728ba86a8f4
AymericSANNA <sanna.huet@gmail.com>
2022-09-26 22:43:37 +0200

    installation des unicons et vérifications des access

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,10 +1,16 @@
-
+// == Npm Imports 
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold text-red-500 bg-black">
+      Hello world!
+    </h1> 
+    <UilReact size={100}/>
+    
     </div>
   );
 }
