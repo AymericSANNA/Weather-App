3113d29c59d8f32132affc14107828e70d819f23
AymericSANNA <sanna.huet@gmail.com>
2022-10-01 16:12:46 +0200

    mise en place de la connexion API weather et dynamisation des données dans timeandlocation

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,31 +1,47 @@
-// == Npm Imports 
-import UilReact from '@iconscout/react-unicons/icons/uil-react';
-import React from 'react';
-// == Local Imports
-import './App.css';
-import Forecast from './components/Forecast';
-import Inputs from './components/Inputs';
-import TemperatureAndDetails from './components/TemperatureAndDetails';
-import TimeAndLocation from './components/TimeAndLocation';
-import TopButtons from './components/TopButtons';
+// == Npm Imports
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import React, { useEffect, useState } from "react";
 
+// == Local Imports
+import "./App.css";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService/weatherService";
 
 // Reuni les different components pour les afficher
 function App() {
-  return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+	const [query, setQuery] = useState({ q: "paris" });
+	const [units, setUnits] = useState("metric");
+	const [weather, setWeather] = useState(null);
 
-    <TopButtons/>
-    <Inputs/>
+	useEffect(() => {
+		const fetchWeather = async () => {
+			await getFormattedWeatherData({ ...query, units }).then((data) => {
+				setWeather(data);
+			});
+		};
 
-    <TimeAndLocation/>
-    <TemperatureAndDetails/>
+		fetchWeather();
+	}, [query, units]);
 
-    <Forecast title="hourly forecast" />
-    <Forecast title="daily forecast" />
-    </div>
+	return (
+		<div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+			<TopButtons />
+			<Inputs />
+			{weather && (
+				<div>
+					<TimeAndLocation weather={weather}/>
+					<TemperatureAndDetails weather={weather} />
 
-  );
+					<Forecast title="hourly forecast" />
+					<Forecast title="daily forecast" />
+				</div>
+			)}
+		</div>
+	);
 }
 
 // == Export
--- my-weather-app/src/components/TimeAndLocation/index.js
+++ my-weather-app/src/components/TimeAndLocation/index.js
@@ -1,19 +1,20 @@
 // == Import npm
 import React from "react";
+import { formatToLocalTime } from "../../services/weatherService/weatherService";
 
 // == Component
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return ( <div>
 
     <div className="flex items-center justify-center my-6">
 
     <p className=" text-white text-xl font-extralight "/* Renvoie la date et l'heure*/>
-        Tuesday, 31 May 2022 | Local time: 12:56 PM
+        {formatToLocalTime(dt,timezone)}
     </p>      
 
     </div>
     <div className="flex items-center justify-center my-3"/* Renvoie la ville */>
-        <p className="text-white text-3xl font-meduim">Berlin, DE</p>
+        <p className="text-white text-3xl font-meduim">{name} {country}</p>
     </div>
   </div>
   )
